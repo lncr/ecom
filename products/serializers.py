75d09e4820e36b0a6672ca5dae023d5f843a1cf0
Bektur <b.soltobaev@disoft.dev>
2021-11-03 21:04:11 +0600

    Add aggregate funcs

--- products/serializers.py
+++ products/serializers.py
@@ -1,13 +1,41 @@
+from django.db.models import Max, Sum, Avg
 from rest_framework import serializers
 
 from products.models import Product
+
+
+def generate_name():
+    number = Product.objects.aggregate(Avg('id')) + 1
+    return f'Product#{number}'
 
 class ProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'price', 'amount', 'rating']
+        fields = ['id', 'name', 'price', 'amount', ]
         extra_kwargs = {'name': {'required': False}}
+
+    def create(self, validated_data):
+        if not validata_data.get('name'):
+            name = generate_name()
+            validated_data['name'] = name
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        pass
+    
+    def delete(self, instance):
+        pass
+
+# api/v1/products/<id>/     PATCH / PUT    DELETE    GET
+
+
+# retrieve
+# list
+# create
+# update
+# delete
+
 
 
 class AmountSerializer(serializers.Serializer):
--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from products.models import Product
 
-# Create your tests here.
+
+User = get_user_model()
+
+
+class ProductsTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='test_user', password='test1234')
+        self.product1 = Product.objects.create(name='Apple', amount=100, price=50)
+        self.product2 = Product.objects.create(name='Notebook', amount=5, price=1000)
+
+    def test_list_products(self):
+        url = reverse('product-list')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_cart_adding_unauthorized(self):
+        url = reverse('product-cart', kwargs={'pk': self.product1.id})
+        data = {'amount': 5}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_cart_adding_authorized(self):
+        url = reverse('product-cart', kwargs={'pk': self.product1.id})
+        data = {'amount': 5}
+        self.client.force_authenticate(self.user)
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
